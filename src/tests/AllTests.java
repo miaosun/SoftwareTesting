db3ebaf00c55163075116aae5fb9ab6a9d14fabe
miaosun <miaosun88@gmail.com>
2015-01-30 02:23:20 +0000

    JUnit

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -1,4 +1,4 @@
-package unitTest;
+package tests;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
--- src/tests/FactoryTestSuite.java
+++ src/tests/FactoryTestSuite.java
@@ -1,4 +1,4 @@
-package unitTest;
+package tests;
 
 import static org.junit.Assert.*;
 
